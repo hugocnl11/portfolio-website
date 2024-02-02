baf7577ab03bba27e3383f503429f3816dce3038
hugocnl11 <102023438+hugocnl11@users.noreply.github.com>
2024-02-02 18:58:55 +0100

    Update voxel-dog.js

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -15,7 +15,7 @@ const VoxelDog = () => {
 
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/dog.glb'
+  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://hugog-portfolio.vercel.app' : '') + '/dog.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
